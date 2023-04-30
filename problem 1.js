ae4c4587f4e4351d41af52254d4197237e70b436
Harsh Gupta <harshguptaaa7@gmail.com>
2023-04-30 21:41:11 +0530

    Day 1 program

---
+++ problem 1.js
@@ -0,0 +1,6 @@
+let Harsh = 10;
+console.log(Harsh);
+Harsh = "Harsh";
+console.log(Harsh);
+Harsh = "gupta";
+console.log(Harsh)
---
+++ problem 3.js
@@ -0,0 +1,4 @@
+let Name = "Harsh Gupta";
+let Age = 24;
+console.log(Name, Age);
+console.log(typeof(Name), typeof(Age));
